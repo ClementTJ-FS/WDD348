3a9d1b6973951a429a3565af56470d59ca78eda7
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2022-03-01 21:30:13 -0800

    Initial commit. - Create React boiler plate.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WDD348
 Project and Portfolio IV: Web Design and Development 
+
+
+## Dev Branch -
+The working branch for the application.
+
+### Changes -
+- Created initial React boiler plate.
+- Replaced default data with app information.
---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header><h1 style={styles.h1}>GamesFree</h1></header>
+     
+    </div>
+  );
+}
+
+export default App;
+
+const styles = {
+  h1: {
+    textAlign: 'center'
+  }
+}
